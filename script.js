8cb7f2b858106c3b8974402bf7cfe3c7a996de7d
Shafay Abid <shafayabid4@gmail.com>
2024-03-09 14:28:39 +0500

    mouse hover effect

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 
 const grid = document.querySelector(".grid");
 
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < 10; i++) {
+    
     const row = document.createElement("div");
     row.style.display = "flex";
     row.style.flex = 1;
-    for (let j = 0; j < 15; j++){
+
+    for (let j = 0; j < 10; j++){
+
         const box = document.createElement("div");
         box.style.borderStyle = "solid";
         box.style.flex = 1;
         box.style.boxSizing = "border-box";
+
+        box.addEventListener("mouseover", function (e) {
+            e.target.style.background = "black";
+          });
+
         row.appendChild(box);
     }
+
     grid.appendChild(row)
+
 }
